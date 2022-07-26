a29eec3d6dea68acf6ba883e6b4c936ddb3984b6
george <lorente.jorgemartin@gmail.com>
2022-07-26 15:52:52 -0300

    se agrega proyecto

---
+++ clase3Lorente.js
@@ -0,0 +1,10 @@
+
+let respuestaDelUsuario 
+
+
+
+while(respuestaDelUsuario != 4){
+    pregunta=parseInt(prompt("hola bienvenido al desafio de la clase3, cuanto es 2+2?"))
+    if (pregunta==4){alert("felcidades esa es la respuesta correcta")}
+    respuestaDelUsuario=pregunta
+}
